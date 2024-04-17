126223024f1640ab9d68538dafee72513eff4085
Vojtech Machacek <vmachacek@gmail.com>
2024-04-17 01:45:27 -0400

    Remove clientside

--- front-end/src/app/models/countryModel.ts
+++
@@ -1,9 +0,0 @@
-export class CountryModel {
-    id: number;
-    name: String;
-    description: String;
-    capitalCity: String;
-    anthem: String;
-    languages: String[];
-    flagUri: String;
-}
